d38d84457f187bfb4f9e11a76405b627fff95495
Aundra Miller and Ronando <ronando.long@gmail.com>
2016-02-17 14:56:35 -0800

    It works. It will take user input for sentence, target word and replacement word and spit out accordingly. win.

--- app/app.php
+++ app/app.php
@@ -1,17 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Contact.php";
-        // If you hit the BSOD (blank screen of death) in a Silex app, add these lines to your app.php file right under your require_once statements.
-        //Add symfony debug component and turn it on.
-            //use Symfony\Component\Debug\Debug;
-            //Debug::enable();
-    
-        // Set Silex debug mode in $app object
-            //$app['debug'] = true;
-    session_start();
-    if (empty($_SESSION['list_of_places'])) {
-        $_SESSION['list_of_places'] = array();
-    }
+    require_once __DIR__."/../src/FindAndReplace.php";
 
     $app = new Silex\Application();
 
@@ -23,10 +12,17 @@
 
     // Render Home Page
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('example.html.twig'); //
+        return $app['twig']->render('findandreplace.html.twig'); //
+    });
+
+    // User Input from form
+    $app->get("/userInput", function() use ($app) {
+        $new_phrase = new FindAndReplace;
+        $result = $new_phrase->swapWords($_GET['sentence'], $_GET['find'], $_GET['change']);
+
+        return $app['twig']->render('findandreplace.html.twig', array('result' => $result)); //
     });
 
     return $app;
 
 ?>
-
